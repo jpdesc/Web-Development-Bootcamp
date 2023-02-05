44bdf21f5b14a3f0460f5e120f6b7e098bdf9906
Jack DesCombes <jpdesc@alumni.stanford.edu>
2023-02-04 18:05:48 -0800

    Finish JQuery module

--- DOM/JQuery/index.js
+++ DOM/JQuery/index.js
@@ -1,1 +1,11 @@
-$("h1").css("color", "red");
+$("input").keydown(function (event) {
+  $("h1").text(event.key);
+});
+
+$("h1").on("mouseover", function () {
+  $("h1").css("color", "red");
+});
+
+$("button").on("click", function () {
+  $("h1").slideUp().slideDown().animate({ opacity: 0.5 });
+});
